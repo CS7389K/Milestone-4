b8435da5c3a4ab94523ed6ec33a0e3e6dc0d2fd1
HalkScout <andrewscouten@gmail.com>
2025-10-31 11:17:29 -0500

    Setup subscriber / publisher / packages

--- src/milestone4/setup.py
+++ src/milestone4/setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 package_name = 'milestone4'
 
 setup(
-    name=package_name,
-    version='0.0.0',
+    name=milestone4,
+    version='0.0.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,13 +13,15 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='andrew',
-    maintainer_email='andrew@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Andrew Scouten',
+    maintainer_email='yzb2@txstate.edu',
+    description='Real-time object detection using the YOLO model with the TurtleBot3.',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'talker = milestone4.publisher:main',
+            'listener = milestone4.subscriber:main',
         ],
     },
 )
